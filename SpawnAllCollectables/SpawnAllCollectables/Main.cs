3af8ae80a5aaa5b19b28fa00ac91c97b2acec3eb
yowchap <50385582+yowchap@users.noreply.github.com>
2021-12-01 19:38:37 -0800

    Added project files

---
+++ SpawnAllCollectables/SpawnAllCollectables/Main.cs
@@ -0,0 +1,82 @@
+﻿using MelonLoader;
+using ModThatIsNotMod;
+using ModThatIsNotMod.BoneMenu;
+using StressLevelZero.Data;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using System.Linq;
+
+namespace YOWC.SpawnAllCollectables
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Spawn All Collectables"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "YOWChap"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class Main : MelonMod
+    {
+        private List<SpawnableObject> collectables = new List<SpawnableObject>();
+        private Vector3 randomOffset => new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(-0.5f, 1f), Random.Range(-2.5f, 2.5f));
+
+        // These ones have item balls instead
+        private string[] blockedItems = new string[]
+        {
+            "Monitor",
+            "Shopping Cart",
+            "Melon",
+            "Plant",
+            "Wooden Crate Destructable",
+            "Indestructable 2m Crate",
+            "Hexagonal Container",
+            "Concrete Barrier",
+            "Cardboard Box",
+            "Indestructable 1m Crate",
+            "Pallet Jack",
+            "Vehicle OmniWay",
+            "The Wasp"
+        };
+
+
+        public override void OnApplicationStart()
+        {
+            MenuCategory category = MenuManager.CreateCategory("Collectables Spawner", Color.cyan);
+            category.CreateFunctionElement("Spawn All Collectables", Color.white, new Action(SpawnCollectables));
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            if (buildIndex == 1) // Main menu
+            {
+                UnlockableDisplayCase displayCase = GameObject.FindObjectOfType<UnlockableDisplayCase>();
+                if (displayCase != null)
+                {
+                    collectables.AddRange(displayCase.spawn_fantasy);
+                    collectables.AddRange(displayCase.spawn_melee);
+                    collectables.AddRange(displayCase.spawn_misc.Where(x => !x.title.ToLower().Contains("gym ") && !blockedItems.Contains(x.title)).ToArray());
+                    collectables.AddRange(displayCase.spawn_physics.Where(x => !blockedItems.Contains(x.title)).ToArray());
+                    collectables.AddRange(displayCase.spawn_ranged);
+                }
+
+                SpawnableObject[] itemBalls = Resources.FindObjectsOfTypeAll<SpawnableObject>().Where(x => x.title.ToLower().Contains("item ball") || x.title.ToLower().Contains("capsule ")).ToArray();
+                collectables.AddRange(itemBalls);
+            }
+        }
+
+        private void SpawnCollectables()
+        {
+            Vector3 headPos = Player.GetPlayerHead().transform.position;
+            foreach (SpawnableObject spawnable in collectables)
+            {
+                GameObject newObj = GameObject.Instantiate(spawnable.prefab, headPos + randomOffset, Quaternion.identity);
+                newObj.SetActive(true);
+            }
+            MenuManager.CloseMenu();
+        }
+    }
+}
---
+++ SpawnAllCollectables/SpawnAllCollectables/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(YOWC.SpawnAllCollectables.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(YOWC.SpawnAllCollectables.BuildInfo.Company)]
+[assembly: AssemblyProduct(YOWC.SpawnAllCollectables.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + YOWC.SpawnAllCollectables.BuildInfo.Author)]
+[assembly: AssemblyTrademark(YOWC.SpawnAllCollectables.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(YOWC.SpawnAllCollectables.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(YOWC.SpawnAllCollectables.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(YOWC.SpawnAllCollectables.Main), YOWC.SpawnAllCollectables.BuildInfo.Name, YOWC.SpawnAllCollectables.BuildInfo.Version, YOWC.SpawnAllCollectables.BuildInfo.Author, YOWC.SpawnAllCollectables.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Stress Level Zero", "BONEWORKS")]
